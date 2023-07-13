f549b7534ff1b0cb7ffb209697bf579c06bedda2
Tatsuya Kosuge <castero1219@gmail.com>
2023-07-14 04:56:33 +0900

    [add]ログイン機能を実装

--- app/components/common/LogoutForm.tsx
+++ app/components/common/LogoutForm.tsx
@@ -8,7 +8,7 @@ const LogoutForm = ({ user }) => {
           {user.username}
           <button
             type="submit"
-            className="w-full flex items-center justify-center gap-2 rounded-lg bg-red-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-red-300 transition duration-100 hover:bg-red-600 md:text-base"
+            className="w-full flex items-center justify-center gap-2 border-0 rounded-lg bg-blue-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-red-300 transition duration-100 hover:bg-red-600 md:text-base"
           >
             Logout
           </button>
